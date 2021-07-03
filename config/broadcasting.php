e44849c97eaf2f74b0ea00d4c919dbaffe28a6c8
SaiPha454 <cencer880@gmail.com>
2021-07-03 13:47:08 +0630

    eleventh commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,10 +37,14 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'useTLS' => true,
-                'host' => "127.0.0.1",
-                'port' => 6001,
-                'scheme' => 'https'
+                'useTLS' => false,
+                'host' => "sockets.". env("APP_HOST"),
+                'port' => 443,
+                'scheme' => 'https',
+                'curl_options' => [
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                ]
             ],
         ],
 
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -30,8 +30,9 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
-    wsPort: 6001,
+    wsPort: 443,
     forceTLS: false,
     disableStats: true,
+    enabledTransports:['ws','wws']
 
 });
