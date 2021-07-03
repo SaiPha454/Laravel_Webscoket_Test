2b3c522f0889b2a253dba4ae48a74caca6ae01b9
SaiPha454 <cencer880@gmail.com>
2021-07-03 13:21:36 +0630

    tenth commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -38,9 +38,9 @@ return [
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-                'host' => env('APP_HOST'),
+                'host' => "127.0.0.1",
                 'port' => 6001,
-                'scheme' => 'http'
+                'scheme' => 'https'
             ],
         ],
 
